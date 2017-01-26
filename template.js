e6d7ddefc029ad23c1ae32d8252a8958014e4aa4
Justin Hyland <j@linux.com>
2017-01-26 13:06:58 -0700

    first commit - just testing

--- template.js
+++ template.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Test Script
 // @namespace    http://liberty-sweet.ldcstaging.com/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        http://liberty-sweet.ldcstaging.com/*
@@ -12,5 +12,6 @@
   'use strict';
   console.debug( 'Script loaded!' );
   console.debug( 'Hello World' );
+  console.debug( 'WTF?!' );
   // Magic here
 })();
