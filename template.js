fc5c02cb39a19d0e6e3bd9d93c5ee29ea112642b
Justin Hyland <j@linux.com>
2017-01-26 12:58:17 -0700

    first commit - just testing

--- template.js
+++ template.js
@@ -1,15 +1,16 @@
 // ==UserScript==
-// @name         New Userscript
-// @namespace    http://tampermonkey.net/
+// @name         Test Script
+// @namespace    http://liberty-sweet.ldcstaging.com/
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        http://*/*
+// @match        http://liberty-sweet.ldcstaging.com/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
   'use strict';
   console.debug( 'Script loaded!' );
+  console.debug( 'Hello World' );
   // Magic here
 })();
